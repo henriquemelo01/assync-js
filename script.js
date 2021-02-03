4d448a29fe223f55154ce7a661c553009befc07e
Henrique Moraes <henriquemsm@outlook.com>
2021-02-03 18:49:14 -0300

    throw new Error()

--- script.js
+++ script.js
@@ -3,22 +3,6 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
-
-/* ASYNC JS: Os códigos assincronos são executados em paralelo aos códigos sincronos, assim sua execução não precisa ser finalizada para o código sincrono ser executado. Ex: setTimeout(function,2000).
-
-Os códigos assincronos estão presentes na chamada de API'S através do AJAX (Assyncronos JS and XML):
-
-As APIS são ,basicamente, um pedaço de um software que pode ser aproveitado por um outro pedaço de software, permitindo a troca de informação entre dois sistemas.
-
-// AJAX: Nos permite comunicarmos com um web server de maneira assincrona. Com as chamadas AJAX podemos solicitar dados de um web server de maneira dinâmica (sem recarregar página)
-
-Client -> HTTP Request (GET - get Data, POST - send data, etc...) 
-Web Server (Normalmente, uma API) -> Response (Send data back)
-
-*/
-
-// countryName receives "" for the current country e "neighbor" for the country neighbor
 const renderContries = function (data, countryName) {
   const html = `
     <article class="country ${countryName}">
@@ -37,6 +21,27 @@ const renderContries = function (data, countryName) {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', `${msg}`);
+};
+
+///////////////////////////////////////
+
+/* ASYNC JS: Os códigos assincronos são executados em paralelo aos códigos sincronos, assim sua execução não precisa ser finalizada para o código sincrono ser executado. Ex: setTimeout(function,2000).
+
+Os códigos assincronos estão presentes na chamada de API'S através do AJAX (Assyncronos JS and XML):
+
+As APIS são ,basicamente, um pedaço de um software que pode ser aproveitado por um outro pedaço de software, permitindo a troca de informação entre dois sistemas.
+
+// AJAX: Nos permite comunicarmos com um web server de maneira assincrona. Com as chamadas AJAX podemos solicitar dados de um web server de maneira dinâmica (sem recarregar página)
+
+Client -> HTTP Request (GET - get Data, POST - send data, etc...) 
+Web Server (Normalmente, uma API) -> Response (Send data back)
+
+*/
+
+// countryName receives "" for the current country e "neighbor" for the country neighbor
 
 // const getCountryInfo = function (country) {
 //   const request = new XMLHttpRequest();
@@ -67,7 +72,7 @@ const renderContries = function (data, countryName) {
 // const getCountryAndNeighbor = function (country) {
 //   const request = new XMLHttpRequest();
 
-//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`); // GET HTTP request
 //   request.send();
 
 //   request.addEventListener('load', function () {
@@ -103,25 +108,29 @@ const renderContries = function (data, countryName) {
 
 */
 
-const getCountry = function (country) {
-  // return promisses - Objeto que contém dados sobre o request, then (Request foi aceito e retornou uma promisses com os dados solicitados)
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(
-      response => response.json(),
-      err => console.log(err.message)
-    ) // o metodo .json() retorna uma promisse que contém os dados obtidos no fetch
-    .then(data => {
-      renderContries(data[0], '');
-      const [neighbour] = data[0].borders;
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-    })
-    .then(response => response.json())
-    .then(data => renderContries(data, 'neighbour'))
-    .catch(err => {
-      countriesContainer.insertAdjacentText('afterbegin', `${err.message}`);
-    }) // identifica error de maneira global, ou seja, erros que ocorrem em toda chain promisse.
-    .finally(() => (countriesContainer.style.opacity = 1)); // função é executada sempre, independendo do resultado da requisição (failed or sucess)
-};
+// Promises: A promise é um objeto que representa a eventual conclusão ou falha de uma operaçaõ assincrona. É um objeto que armazena o resultado de uma operação assincrona
+
+// const getCountry = function (country) {
+//   // return promises - Objeto que contém dados sobre o request, then (Request foi aceito e retornou uma promises com os dados solicitados).
+
+//   // fetch -> build a promise
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(
+//       response => response.json(),
+//       // OBS: HTTP response, has a body that contains a JSON if we get the response of an API or HTML in case of we get a web page.
+//       err => console.log(err.message)
+//     ) // .json parsing the body text as JSON.
+//     .then(data => {
+//       // console.log(data); // Retorna um array que contém o objeto que esta sendo solicitado no fetch
+//       renderContries(data[0], '');
+//       const [neighbour] = data[0].borders;
+//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//     })
+//     .then(response => response.json()) // parsing the body text as JSON.
+//     .then(data => renderContries(data, 'neighbour'))
+//     .catch(() => renderError()) // identifica error de maneira global, ou seja, erros que ocorrem em toda chain promisse.
+//     .finally(() => (countriesContainer.style.opacity = 1)); // função é executada sempre, independendo do resultado da requisição (failed or sucess)
+// };
 
 // Handle errors in promisses só são disparados quando o usuário perde a conexão de internet - promisses não foram preenchidas
 
@@ -136,5 +145,37 @@ Promisses.finally(): Metodo da promisses que sempre executa a callback, independ
 */
 
 btn.addEventListener('click', function () {
-  getCountry('Brazil');
+  getCountry('Australia');
+  countriesContainer.innerHTML = '';
 });
+
+// A callback do erro não é disparada se fizermos um fetch de uma pagina que não existe/não pode ser encontrada - 404, para corrig"irmos tal limitação, vamos definir a callback para este erro manualmente. In this case, the propertie ok of the promise that was returned by fetch() is false.
+
+// getCountry('aosaskapsk'); // Page cannot be found
+
+const getJSON = function (url, errMsg = 'Something went wrong 💣💣 : ') {
+  return fetch(`${url}`).then(response => {
+    // Page not found (404)
+    if (!response.ok) throw new Error(`${errMsg}`); // throw -> se linha for executada muda status da promise para failed, seta err.msg
+
+    return response.json();
+  });
+};
+
+const getCountry = function (country) {
+  // fetch -> build a promise
+  getJSON(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(data => {
+      renderContries(data[0], '');
+      const [neighbour] = data[0].borders;
+
+      if (!neighbour) throw new Error('There are no neighbors in this country');
+
+      return getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(data => renderContries(data, 'neighbour'))
+    .catch(err => renderError(`Something went wrong 💣💣 : ${err.message}`))
+    .finally(() => (countriesContainer.style.opacity = 1));
+};
+
+// getCountry('asaoksap');
